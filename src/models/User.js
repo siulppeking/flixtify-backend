4acd0d8676212670734a3c9832560f33389a151b
siulppeking <elchancateclas@gmail.com>
2025-12-27 23:53:08 -0500

    refactor: extract constants from User model schema

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
+// Constants for model defaults and enums
+const BCRYPT_SALT_ROUNDS = 10;
+
+const THEME_ENUM = ['light', 'dark', 'system'];
+const THEME_DEFAULT = 'system';
+
+const FONT_SIZE_ENUM = ['sm', 'base', 'lg', 'xl'];
+const FONT_SIZE_DEFAULT = 'base';
+
+const FONT_FAMILY_ENUM = ['sans', 'serif', 'mono'];
+const FONT_FAMILY_DEFAULT = 'sans';
+
+const COLOR_SCHEME_ENUM = ['default', 'red', 'green', 'blue', 'purple'];
+const COLOR_SCHEME_DEFAULT = 'default';
+
 const UserSchema = new mongoose.Schema(
   {
     username: {
@@ -36,23 +51,23 @@ const UserSchema = new mongoose.Schema(
     userPreferences: {
       theme: {
         type: String,
-        enum: ['light', 'dark', 'system'],
-        default: 'system'
+        enum: THEME_ENUM,
+        default: THEME_DEFAULT
       },
       fontSize: {
         type: String,
-        enum: ['sm', 'base', 'lg', 'xl'],
-        default: 'base'
+        enum: FONT_SIZE_ENUM,
+        default: FONT_SIZE_DEFAULT
       },
       fontFamily: {
         type: String,
-        enum: ['sans', 'serif', 'mono'],
-        default: 'sans'
+        enum: FONT_FAMILY_ENUM,
+        default: FONT_FAMILY_DEFAULT
       },
       colorScheme: {
         type: String,
-        enum: ['default', 'red', 'green', 'blue', 'purple'],
-        default: 'default'
+        enum: COLOR_SCHEME_ENUM,
+        default: COLOR_SCHEME_DEFAULT
       }
     },
     lastLoginAt: Date,
@@ -72,7 +87,7 @@ UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     return next();
   }
-  const salt = await bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(BCRYPT_SALT_ROUNDS);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
