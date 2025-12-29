35b486f2e48df9932b0d55f87d4f624ede9ef751
Siulppe King <elchancateclas@gmail.com>
2025-12-29 16:47:29 -0500

    refactor(model): add TTL index and unique constraint to Token schema

--- src/models/Token.js
+++ src/models/Token.js
@@ -5,20 +5,25 @@ const TokenSchema = new mongoose.Schema(
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
-      required: true
+      required: true,
+      index: true
     },
     refreshToken: {
       type: String,
-      required: true
+      required: true,
+      unique: true
     },
     userAgent: String,
     ip: String,
     expiresAt: {
       type: Date,
-      required: true
+      required: true,
+      index: true
     }
   },
   { timestamps: true }
 );
+
+TokenSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
 
 module.exports = mongoose.model('Token', TokenSchema);
