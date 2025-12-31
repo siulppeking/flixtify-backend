f9090396b587f2939799bbda14894c61a34eb4b9
Siulppe King <elchancateclas@gmail.com>
2025-12-31 09:30:03 -0500

    refactor(constants): add HTTP status code constants

---
+++ src/constants/httpStatus.js
@@ -0,0 +1,23 @@
+/**
+ * HTTP Status Code Constants
+ * Centralized definitions for all HTTP status codes used across the API
+ */
+
+module.exports = {
+  // Success responses
+  OK: 200,
+  CREATED: 201,
+  ACCEPTED: 202,
+  NO_CONTENT: 204,
+
+  // Client error responses
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+
+  // Server error responses
+  INTERNAL_SERVER_ERROR: 500,
+  SERVICE_UNAVAILABLE: 503
+};
