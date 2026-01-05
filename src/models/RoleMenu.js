6a773358dd3438f027707c13a788a2f1fb8ab296
siulppeking <elchancateclas@gmail.com>
2026-01-04 19:57:47 -0500

    refactor: add constants and JSDoc to RoleMenu model

--- src/models/RoleMenu.js
+++ src/models/RoleMenu.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose');
 
+// Constants for RoleMenu model
+const ROLE_MENU_FIELDS = {
+  ROLE_ID: 'roleId',
+  MENU_ID: 'menuId'
+};
+
+/**
+ * RoleMenu schema links roles with menus
+ */
 const RoleMenuSchema = new mongoose.Schema(
   {
-    roleId: {
+    [ROLE_MENU_FIELDS.ROLE_ID]: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Role',
       required: true
     },
-    menuId: {
+    [ROLE_MENU_FIELDS.MENU_ID]: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Menu',
       required: true
@@ -15,7 +24,7 @@ const RoleMenuSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
-    indexes: [{ unique: true, fields: ['roleId', 'menuId'] }]
+    indexes: [{ unique: true, fields: [ROLE_MENU_FIELDS.ROLE_ID, ROLE_MENU_FIELDS.MENU_ID] }]
   }
 );
 
