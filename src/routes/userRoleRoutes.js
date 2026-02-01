2fec456902056fc5f6194144eb7f5dcde382a949
siulppeking <elchancateclas@gmail.com>
2026-01-31 22:55:58 -0500

    Refactor user-role routes with constants and JSDoc

--- src/routes/userRoleRoutes.js
+++ src/routes/userRoleRoutes.js
@@ -5,8 +5,29 @@ const userRoleController = require('../controllers/userRoleController');
 const auth = require('../middlewares/authMiddleware');
 const checkAdmin = require('../middlewares/checkAdmin');
 
-router.post('/', auth, checkAdmin, userRoleController.assignRoleToUser);
-router.get('/:userId', auth, userRoleController.getRolesByUser);
-router.delete('/:userId/:roleId', auth, checkAdmin, userRoleController.revokeRoleFromUser);
+// Route path constants
+const USER_ROLE_ROUTES = {
+  ROOT: '/',
+  BY_USER: '/:userId',
+  BY_USER_AND_ROLE: '/:userId/:roleId'
+};
+
+/**
+ * POST /api/user-roles - Assign role to user
+ * @description Assign a role to a user (requires authentication and admin privilege)
+ */
+router.post(USER_ROLE_ROUTES.ROOT, auth, checkAdmin, userRoleController.assignRoleToUser);
+
+/**
+ * GET /api/user-roles/:userId - Get roles by user
+ * @description Retrieve all roles assigned to a user (requires authentication)
+ */
+router.get(USER_ROLE_ROUTES.BY_USER, auth, userRoleController.getRolesByUser);
+
+/**
+ * DELETE /api/user-roles/:userId/:roleId - Revoke role from user
+ * @description Remove a role from a user (requires authentication and admin privilege)
+ */
+router.delete(USER_ROLE_ROUTES.BY_USER_AND_ROLE, auth, checkAdmin, userRoleController.revokeRoleFromUser);
 
 module.exports = router;
