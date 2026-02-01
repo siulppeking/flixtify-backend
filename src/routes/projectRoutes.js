5e9c14e5924e2917f97f8e50cb74cf67e2eaa357
siulppeking <elchancateclas@gmail.com>
2026-01-31 22:53:37 -0500

    Refactor project routes with constants and JSDoc

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -5,13 +5,40 @@ const projectController = require('../controllers/projectController');
 const auth = require('../middlewares/authMiddleware');
 const { validateProjectCreation } = require('../validators/projectValidator');
 
-router.route('/')
-  .post(auth, validateProjectCreation, projectController.createProject)
-  .get(auth, projectController.getAllProjects);
+// Route path constants
+const PROJECT_ROUTES = {
+  ROOT: '/',
+  BY_ID: '/:id'
+};
 
-router.route('/:id')
-  .get(auth, projectController.getProjectById)
-  .put(auth, projectController.updateProject)
-  .delete(auth, projectController.deleteProject);
+/**
+ * POST /api/projects - Create new project
+ * @description Create a new project (requires authentication)
+ */
+router.post(PROJECT_ROUTES.ROOT, auth, validateProjectCreation, projectController.createProject);
+
+/**
+ * GET /api/projects - Get all projects
+ * @description Retrieve all projects (requires authentication)
+ */
+router.get(PROJECT_ROUTES.ROOT, auth, projectController.getAllProjects);
+
+/**
+ * GET /api/projects/:id - Get project by ID
+ * @description Retrieve a single project by ID (requires authentication)
+ */
+router.get(PROJECT_ROUTES.BY_ID, auth, projectController.getProjectById);
+
+/**
+ * PUT /api/projects/:id - Update project
+ * @description Update project information (requires authentication)
+ */
+router.put(PROJECT_ROUTES.BY_ID, auth, projectController.updateProject);
+
+/**
+ * DELETE /api/projects/:id - Delete project
+ * @description Delete a project (requires authentication)
+ */
+router.delete(PROJECT_ROUTES.BY_ID, auth, projectController.deleteProject);
 
 module.exports = router;
